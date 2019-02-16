757b8b4a12e80b1f2422dafaecb2b1eb019f5c0c
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-02-16 21:14:52 +0700

    initial commit

---
+++ Authentications/BasicAuthenticationSchema.cs
@@ -0,0 +1,7 @@
+namespace WebApiBasic.Authentications
+{
+    public static class BasicAuthenticationSchema
+    {
+        public static string AuthenticationScheme = "Basic";
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# aspcore-basic-authentication
+study project.
+
+### USAGE
+```
+http://localhost:5000/api/values
+Username: admin
+Password: admin
+```
